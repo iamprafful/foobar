50c9e44068a19e38cea41d713e7e9b2c582d5570
Prafful Lachhwani <prafful.lachhwani@gmail.com>
2020-04-23 21:23:24 +0530

    Level 4 Solution 1

--- Level 4/free-the-bunny-prisoners/Solution.java
+++ Level 4/free-the-bunny-prisoners/Solution.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) {
-        int arr[][]=solution(4, 4);
+        int arr[][]=solution(5, 3);
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 System.out.print(arr[i][j]+" ");
